c20e6723e34c3397b7bab27542561210d4489a0c
Esteban Lopez <estebanlopez.cn@gmail.com>
2020-09-15 23:32:34 -0500

    feat: add Ingredients dinamically

--- src/components/containers/BurgerBuilder/BurgerBuilder.js
+++ src/components/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,10 +3,19 @@ import AuxWrapper from '../../../highOrderComponents/AuxWrapper';
 import Burger from '../../Burger/Burger';
 
 class BurgerBulder extends Component {
+    state = {
+        ingredients: {
+            salad: 0,
+            bacon: 0,
+            cheese: 0,
+            meat: 0
+        }
+    }
+
     render () {
         return (
             <AuxWrapper>
-                <Burger />
+                <Burger ingredients={ this.state.ingredients } />
                 <div>Build controls</div>
             </AuxWrapper>
         );      
